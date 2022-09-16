36205325382a39222ce4646fb47e6c92ed11f9b1
Tribhuvn <111028986+Tribhuvn@users.noreply.github.com>
2022-09-16 23:17:50 +0530

    Update models.py

--- blog_project1/blog/models.py
+++ blog_project1/blog/models.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import NULL
+#from asyncio.windows_events import NULL
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
